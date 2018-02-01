ef3c50686ef45b1ef8d6701099b8186cde7ad42a
tanaphol <tanapholw58@email.nu.ac.th>
2018-02-02 05:12:54 +0700

    Ass2.1

--- ass1.py
+++ ass1.py
@@ -7,20 +7,20 @@ app = Flask (__name__)
 api = Api(app) 
 
 parser = reqparse.RequestParser()
-parser.add_argument('dob')
+parser.add_argument('datebirth')
 
 def calculate_age(born):
 		today = date.today()
 		return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 
-class Hello(Resource):
+class Birthday(Resource):
 	def post(self): 
 		args = parser.parse_args()
-		birthdate = args['dob']
+		birthdate = args['datebirth']
 		dob = calculate_age(datetime.strptime(birthdate, '%d-%m-%Y').date())
 		return {"birthdate":birthdate, "Age":dob}
 
-api.add_resource(Hello,'/age')
+api.add_resource(birthday,'/age')
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0',port=5500)
